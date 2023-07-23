36328362b6fb4c8965450272fbadff3c0aa089ed
Trevon Bernard <Trevon@Trevons-MacBook-Pro.local>
2023-07-23 14:24:22 -0400

    Halfway done

--- schemas/bugSchema.js
+++ schemas/bugSchema.js
@@ -1,38 +1,41 @@
 const mongoose = require("mongoose");
 
-const bugSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const bugSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    priority: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    assignedTo: {
+      type: String,
+      required: true,
+    },
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+      required: true,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  priority: {
-    type: String,
-    required: true,
-    enum: ["Low", "Medium", "High"],
-  },
-  status: {
-    type: String,
-    required: true,
-    enum: ["Open", "In Progress", "Resolved"],
-  },
-  type: {
-    type: String,
-    required: true,
-    enum: ["Bug", "Feature", "Enhancement"],
-  },
-  assignedTo: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User", // Reference to User schema, assuming you have a User schema defined
-  },
-  project: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Project", // Reference to Project schema, assuming you have a Project schema defined
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Bug = mongoose.model("Bug", bugSchema);
 
